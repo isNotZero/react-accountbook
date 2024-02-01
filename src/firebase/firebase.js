1179270d93263af3d790637555b61d23505a3101
isnotzero <isnotzero89@gmail.com>
2024-02-02 00:11:13 +0900

    firebase 추가

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDG4_cpL7H63teQSJELkQakgBX-C7HFGO4",
+  authDomain: "react-accountbook-d1fab.firebaseapp.com",
+  projectId: "react-accountbook-d1fab",
+  storageBucket: "react-accountbook-d1fab.appspot.com",
+  messagingSenderId: "1034084333",
+  appId: "1:1034084333:web:e213269a4ddfa5b39091e4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
