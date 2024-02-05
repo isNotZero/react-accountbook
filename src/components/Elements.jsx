16006e0c33ea0be462eb07a86244f93a2e59a2b2
isnotzero <isnotzero89@gmail.com>
2024-02-05 16:05:09 +0900

    file component 추가

--- src/components/Elements.jsx
+++ src/components/Elements.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from "react"
+
 export function ElInput({ type, label, value, onInput }) {
   return (
     <div className="flex justify-between p-4 bg-gray-300 rounded">
@@ -8,10 +10,26 @@ export function ElInput({ type, label, value, onInput }) {
 }
 
 export function ElFile({ label, onInput }) {
+  const [fileName, setFileName] = useState('')
+  const inputFile = useRef(null)
+
+  function onClickAddBtn() {
+    inputFile.current.click()
+  }
+
+  function onInputFile(e) {
+    setFileName(() => e.target.files[0].name)
+    onInput(e)
+  }
+  
   return (
     <div className="flex justify-between p-4 bg-gray-300 rounded">
       <label htmlFor="">{ label }</label>
-      <input className="rounded border-solid border border-gray-900" type='file' onInput={ onInput }/>
+      <div>
+        <input className="rounded border-solid border border-gray-900" type='text' value={fileName} disabled/>
+        <ElButton onClick={onClickAddBtn}>이미지 선택</ElButton>
+        <input ref={inputFile} className="hidden" type='file' onInput={ onInputFile }/>
+      </div>
     </div>
   )
 }
